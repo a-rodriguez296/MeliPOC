878863924ceb6b2cd12af811d6a7f123751e730e
Alejandro <alejo80805@hotmail.com>
2021-06-28 20:57:20 -0500

    Initial Commit

---
+++ MeliAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeliAssignment
+//
+//  Created by Alejandro Rodríguez on 28/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
