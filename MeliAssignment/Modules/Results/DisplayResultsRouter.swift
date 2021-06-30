6df55e58e979ca402479e1316aed03f8ca001c41
Alejandro <alejo80805@hotmail.com>
2021-06-29 21:26:09 -0500

    Adding storyboard

--- MeliAssignment/Modules/Results/DisplayResultsRouter.swift
+++ MeliAssignment/Modules/Results/DisplayResultsRouter.swift
@@ -6,7 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 class DisplayResultsRouter: DisplayResultsWireframeLogic {
 
+    weak var viewController: UIViewController?
+
+    static func assembleModule() -> UIViewController {
+        let viewController = DisplayResultsViewController()
+        let router = DisplayResultsRouter()
+        router.viewController = viewController
+        return viewController
+    }
 }
